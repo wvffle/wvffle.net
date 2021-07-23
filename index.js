38376baf00b9dcc062cbcf84510f0d62046a3d25
Kasper Seweryn <github@wvffle.net>
2021-07-23 11:54:09 +0000

    Add .well-known matrix server

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ fastify.get('/favicon.ico', async (request, reply) => {
   return fs.readFile('favicon.ico')
 })
 
+fastify.get('/.well-known/matrix/server', async (request, reply) => {
+  return {
+    "m.server": "matrix.wvffle.net:443"
+  }
+})
+
 fastify.get('*', async (request, reply) => {
   reply.type('html')
   return fs.readFile('index.html')
